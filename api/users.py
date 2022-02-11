79d0650e9d3f09e8636866585a68121dce46e50a
Victor Neo <icyisamu@gmail.com>
2022-02-12 02:36:34 +0800

    Working dummy API responses for webfinger and user object

--- api/users.py
+++ api/users.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import APIRouter, Body, Depends, HTTPException, Response, status
+from fastapi.responses import JSONResponse
 from db.db import get_session
 from sqlmodel import Session, select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,7 +10,52 @@ from models.users import User
 router = APIRouter()
 
 
-@router.get("/users/{user_id}")
+class JSONLDResponse(JSONResponse):
+    media_type = "application/activity+json"
+
+
+base_url = ''
+url = 'https://' + base_url + '/users/vic'
+
+
+@router.get('/.well-known/webfinger')
+async def webfinger():
+    return {
+        "subject": "acct:vic@"+base_url,
+        "aliases": [
+            url,
+        ],
+        "links": [
+            {
+            "rel": "self",
+            "type": "application/activity+json",
+            "href": url
+            },
+        ]
+    }
+
+
+@router.get("/users/{username}", response_class=JSONLDResponse)
+async def get_user(username: str):
+    return {
+        "@context": ["https://www.w3.org/ns/activitystreams"],
+        "type": "Person",
+        "id": url,
+        "following": url + '/following.json',
+        "followers": url + '/followers.json',
+        "liked": url + '/liked.json',
+        "inbox": url + '/inbox.json',
+        "outbox": url + '/feed.json',
+        "preferredUsername": "victorneo",
+        "name": "Victor Neo",
+        "summary": "Dummy profile",
+        "icon": [
+        ],
+    }
+
+
+
+@router.get("/users/id/{user_id}")
 async def get_user(user_id: int, response: Response):
     user = None
     session = await get_session()
--- tests/test_api.py
+++ tests/test_api.py
@@ -16,12 +16,12 @@ def test_read_main():
 
 @pytest.mark.asyncio
 async def test_get_user():
-    response = client.get('/users/91919')
+    response = client.get('/users/id/91919')
     assert response.status_code == 404
     u = User(first_name='a', last_name='b', email='a@b.com', password='123')
     session = await get_session()
     session.add(u)
     await session.commit()
 
-    response = client.get('/users/{}'.format(u.id))
+    response = client.get('/users/id/{}'.format(u.id))
     assert response.status_code == 200
