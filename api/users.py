f99a5a7babdf7ea0d51f8427ad906d3bd2a5b29e
Victor Neo <icyisamu@gmail.com>
2022-02-12 16:45:52 +0800

    Implement proper APIs

--- api/users.py
+++ api/users.py
@@ -14,44 +14,64 @@ class JSONLDResponse(JSONResponse):
     media_type = "application/activity+json"
 
 
-base_url = ''
-url = 'https://' + base_url + '/users/vic'
+domain = ''
+base_url = 'https://' + domain + '/'
 
 
 @router.get('/.well-known/webfinger')
 async def webfinger(resource: str):
-    return {
-        "subject": "acct:vic@"+base_url,
-        "aliases": [
-            url,
-        ],
-        "links": [
-            {
-            "rel": "self",
-            "type": "application/activity+json",
-            "href": url
-            },
-        ]
-    }
+    email = resource.split(':')[1]
+    async with async_session() as s:
+        statement = select(User).where(User.email == email)
+        results = await s.execute(statement)
+        user = results.first()
+    
+    if user:
+        user = user[0]
+        url = base_url + 'users/' + user.username
+        return {
+            "subject": resource,
+            "aliases": [
+                url,
+            ],
+            "links": [
+                {
+                "rel": "self",
+                "type": "application/activity+json",
+                "href": url
+                },
+            ]
+        }
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
 
 
 @router.get("/users/{username}", response_class=JSONLDResponse)
 async def get_user(username: str):
-    return {
-        "@context": ["https://www.w3.org/ns/activitystreams"],
-        "type": "Person",
-        "id": url,
-        "following": url + '/following',
-        "followers": url + '/followers',
-        "liked": url + '/liked',
-        "inbox": url + '/inbox',
-        "outbox": url + '/outbox',
-        "preferredUsername": "victorneo",
-        "name": "Victor Neo",
-        "summary": "Dummy profile",
-        "icon": [
-        ],
-    }
+    async with async_session() as s:
+        statement = select(User).where(User.username == username)
+        results = await s.execute(statement)
+        user = results.first()
+    
+    if user:
+        user = user[0]
+        url = base_url + 'users/' + user.username
+        return {
+            '@context': ["https://www.w3.org/ns/activitystreams"],
+            'type': 'Person',
+            'id': url,
+            'following': url + '/following',
+            'followers': url + '/followers',
+            'liked': url + '/liked',
+            'inbox': url + '/inbox',
+            'outbox': url + '/outbox',
+            'preferredUsername': username,
+            'name': user.first_name,
+            'summary': '',
+            'icon': [],
+        }
+
+    raise HTTPException(status_code=404, detail="User not found")
 
 
 @router.get("/users/id/{user_id}")
